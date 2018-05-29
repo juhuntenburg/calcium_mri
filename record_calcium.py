ab91e9726d7ebd0a3c81e1b8ebc4c2c1f700f498
Julia Huntenburg <ju.huntenburg@gmail.com>
2018-05-29 18:29:02 +0100

    polishing, adding user input

--- record_calcium.py
+++ record_calcium.py
@@ -6,17 +6,17 @@ from matplotlib import animation
 import seaborn as sns
 import numpy as np
 import os
+import time
+import pandas as pd
 
-# set plotting style and define animation function
 sns.set()
 sns.set_style('ticks')
 sns.set_context('talk')
-fig = plt.figure(figsize=(10,5))
-ax1 = fig.add_subplot(1,1,1)
 
 def run_animation():
     running = True
 
+    # define method to stop animation upon click
     def onClick(event):
         nonlocal running
         if running:
@@ -26,6 +26,7 @@ def run_animation():
             anim.event_source.start()
             running = True
 
+    # main animation
     def animate(i):
         xs = np.arange(pmt1_signal.n,pmt1_signal.n+100)
         ys = pmt1_signal.data
@@ -39,20 +40,7 @@ def run_animation():
     fig.canvas.mpl_connect('button_press_event', onClick)
     anim = animation.FuncAnimation(fig, animate, interval=1)
 
-
-
-# Set initial voltage gain value
-pmt1_gain_val = 0.2
-
-# Create an analog output channel with a range of 0-1.25 V range and write out the voltage value set above
-pmt1_gain = Task()
-pmt1_gain.CreateAOVoltageChan("/Dev1/ao0","PMT1_voltage_gain",0,1.25,PyDAQmx.DAQmx_Val_Volts,None)
-pmt1_gain.StartTask()
-pmt1_gain.WriteAnalogScalarF64(1,0,pmt1_gain_val,None)
-pmt1_gain.StopTask()
-pmt1_gain.ClearTask()
-
-# Read voltage from input channel and use callback function to interrupt upon input
+# Class to read voltage from input channel and use callback function to interrupt upon input
 class ReadPMT1(Task):
     def __init__(self):
         Task.__init__(self)
@@ -69,17 +57,67 @@ class ReadPMT1(Task):
         self.a.extend(self.data.tolist())
         self.n += 100
         #print(self.n, self.data[0])
-        return 0 # The function should return an integer
+        return 0
     def DoneCallback(self, status):
         print("Status",status.value)
-        return 0 # The function should return an integer
+        return 0
 
-pmt1_signal = ReadPMT1()
-pmt1_signal.StartTask()
-run_animation()
-plt.show()
-input('Acquiring PMT1 continuously. Press Enter to stop.\n')
-pmt1_signal.StopTask()
-save_array = np.column_stack((np.arange(0, len(pmt1_signal.a)), np.asarray(pmt1_signal.a)))
-np.savetxt(os.path.join(os.curdir,"pmt1_trace.csv"), save_array, delimiter=",")
-pmt1_signal.ClearTask()
+if __name__ == "__main__":
+
+    acq = True
+    first_pass = True
+    gains = []
+    while acq == True:
+        # Ask for voltage gain value
+        pmt1_gain_val = float(input("Enter voltage gain between 0.1 and 1.25 V\n"))
+        print("Setting voltage gain to {0}".format(pmt1_gain_val))
+        # Create an analog output channel with a range of 0-1.25 V range and write out the voltage value set above
+        pmt1_gain = Task()
+        pmt1_gain.CreateAOVoltageChan(b"/Dev1/ao0","PMT1_voltage_gain",0,1.25,PyDAQmx.DAQmx_Val_Volts,None)
+        pmt1_gain.StartTask()
+        print("Acquiring PMT1 continuously")
+        pmt1_gain.WriteAnalogScalarF64(1,0,pmt1_gain_val,None)
+        pmt1_gain.StopTask()
+        pmt1_gain.ClearTask()
+
+        if first_pass == True:
+            # Start the acquisition
+            pmt1_signal = ReadPMT1()
+            pmt1_signal.StartTask()
+            first_pass = False
+        else:
+            gains.extend((len(pmt1_signal.a)-len(gains))*[old_pmt1_gain_val])
+
+        user_input = input('Enter p to plot data, x to stop acquisition or c to change the voltage gain\n')
+
+        while user_input == 'p':
+            # plot and keep waiting for input
+            fig = plt.figure(figsize=(10,5))
+            ax1 = fig.add_subplot(1,1,1)
+            run_animation()
+            plt.show()
+            user_input = input('')
+
+        if user_input == 'c':
+            old_pmt1_gain_val = pmt1_gain_val
+            pass # go back to the beginning of the outer loop
+
+        elif user_input == 'x':
+            acq = False # break the outer loop
+            # stop task
+            print("Stopped acquisition. Acquired {0} data points".format(len(pmt1_signal.a)))
+            pmt1_signal.StopTask()
+            gains.extend((len(pmt1_signal.a)-len(gains))*[pmt1_gain_val])
+            # save data
+            df = pd.DataFrame(np.column_stack((np.arange(0, len(pmt1_signal.a)), np.asarray(pmt1_signal.a), np.asarray(gains))), columns=['timepoint[ms]', 'signal[V]', 'gain[V]'])
+            s = input("To save data, enter absolute file path or press Enter to save data with time stamp in current directory\n")
+            if s == "":
+                timestamp = time.strftime('%Y_%m_%d_%H%M', time.localtime())
+                df.to_csv(os.path.join(os.curdir,"{0}_pmt1_trace.csv".format(timestamp)), sep=",", index=False)
+            else:
+                df.to_csv(s, sep=",", index=False)
+            # clear task
+            pmt1_signal.ClearTask()
+
+        else:
+            input("Invalid input. Enter p to plot data, x to stop acquisition or c to change the voltage gain\n")
